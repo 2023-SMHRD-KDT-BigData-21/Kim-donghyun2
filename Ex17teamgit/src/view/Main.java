929f7c94a6b6987fa10fa0e47bc88fe976502602
user <user@smhrd>
2024-01-15 12:20:49 +0900

    240115 12:20 엄윤준 충돌 도전

--- Ex17teamgit/src/view/Main.java
+++ Ex17teamgit/src/view/Main.java
@@ -1,8 +1,9 @@
 package view;
 
 public class Main {
-public static void main(String[] args) {
-	System.out.println("프로젝트 수정");
-}
+	public static void main(String[] args) {
+		System.out.println("프로젝트 수정");
+		System.out.println("엄윤준 충돌 도전");
 	}
-
+	
+}
