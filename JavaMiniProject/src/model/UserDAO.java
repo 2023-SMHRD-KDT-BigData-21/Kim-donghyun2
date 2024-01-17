d4487750ea430762aba00bd97ce8cb54d463993d
USER <USER@DESKTOP-MVFTAB2>
2024-01-17 14:46:52 +0900

    project start

---
+++ JavaMiniProject/src/model/UserDAO.java
@@ -0,0 +1,29 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class UserDAO {
+	public int Balance() {
+	      //open() 들어갈자리
+	      int bal = 0;
+	      try {
+	         String sql = "SELECT U_BALANCE FROM USER_INFO WHERE U_ID = ?";
+	         Connection conn = DriverManager.getConnection(sql);
+	         PreparedStatement psmt = conn.prepareStatement(sql);
+	         psmt.setString(1, sql);
+	         ResultSet rs =   psmt.executeQuery();
+	         if(rs.next()) {
+	            bal = rs.getInt(5);
+	         }
+	         
+	      } catch (Exception e) {
+	         e.printStackTrace();
+	      }finally {
+	         //close들어갈자리
+	      }
+	      return bal;
+	   }
+}
---
+++ JavaMiniProject/src/model/UserVO.java
@@ -0,0 +1,49 @@
+package model;
+
+public class UserVO {
+   
+   private String id;
+   private String pw;
+   private String name;
+   private int age;
+   
+   private double valance;
+   
+   public UserVO(String id, String pw, String name, int age) {
+      this.id = id;
+      this.pw = pw;
+      this.name = name;
+      this.age = age;
+   }
+   
+   public UserVO(String id, String pw) {
+      this.id = id;
+      this.pw = pw;      
+   }
+   
+   public UserVO(String id) {
+      this.id = id;
+   }
+
+   public String getId() {
+      return id;
+   }
+
+   public String getPw() {
+      return pw;
+   }
+
+   public String getName() {
+      return name;
+   }
+
+   public int getAge() {
+      return age;
+   }
+
+   public double getValance() {
+      return valance;
+   }
+   
+   
+}
