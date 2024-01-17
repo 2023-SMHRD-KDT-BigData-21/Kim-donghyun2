d72c59e7a1ad2aeab6246a9240fa58da6381653f
user <user@smhrd>
2024-01-17 17:30:37 +0900

    240117 1730 윤준 메인 while안에while수정

--- JavaMiniProject/src/view/Main.java
+++ JavaMiniProject/src/view/Main.java
@@ -55,7 +55,7 @@ public class Main {
 
 
 				while (true) {
-					System.out.println("[1]입장 [2]게임 룰 설명 [3]게임 종료");
+					System.out.println("[1]입장 [2]게임 룰 설명 [3]메인으로");
 					System.out.print("메뉴를 선택하세요 : ");
 					int me = sc.nextInt();
 					if (me == 1) {
@@ -63,6 +63,11 @@ public class Main {
 
 					} else if (me == 2) {
 						System.out.println("====룰 설명====");
+					} else if (me == 3) {
+						System.out.println("메인 페이지로 이동합니다.");
+						break;
+					} else {
+						System.out.println("올바른 번호를 입력해주세요.");
 					}
 				}
 
