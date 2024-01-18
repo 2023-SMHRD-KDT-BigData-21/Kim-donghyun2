7909cefe9c5b7d08f8d11a5de8129038c6f62280
daeho204 <ottkiml@gmail.com>
2024-01-18 16:34:01 +0900

    240118 16:33 김대호 main rule 오류 해결 작업진행 가능 SoundController 생성 띵 음악 생성

--- JavaMiniProject/src/view/Main.java
+++ JavaMiniProject/src/view/Main.java
@@ -7,6 +7,7 @@ import controller.BalanceController;
 import controller.RankingCon;
 import controller.RuleCon;
 import controller.SlotCon;
+import controller.SoundCon;
 import controller.joinController;
 import controller.loginController;
 import model.UserVO;
@@ -19,6 +20,8 @@ public class Main {
 
 		System.out.println("====메인 메뉴====");
 		while (true) {
+			SoundCon soc = new SoundCon();
+			soc.playSound("src/audio/금전등록기.wav", false);
 			System.out.println("[1]회원가입 [2]로그인 [3]게임 종료");
 			System.out.print("메뉴를 선택하세요 : ");
 			int menu1 = sc.nextInt();
@@ -60,11 +63,9 @@ public class Main {
 						System.out.println("[1]입장 [2]게임 룰 설명 [3]메인으로");
 						System.out.print("메뉴를 선택하세요 : ");
 						int menu2 = sc.nextInt();
-						
-						if (menu2 == 1) { // 게임선택으로 이동
-						} else if (menu2 == 2) {
-							RuleCon ru = new RuleCon();
-							ru.rule();
+
+						if (menu2 == 1) {
+							// 게임선택으로 이동
 							while (true) {
 								System.out.println("====게임 선택====");
 								System.out.println("[1]슬롯게임 [2]카드게임 [3]잔액확인 [4]랭킹확인 [5]뒤로가기");
@@ -79,16 +80,6 @@ public class Main {
 									// 만약 성공하면 bet * 배당만큼해서 한번 더 업데이트
 									System.out.println();
 									System.out.println("====슬롯게임====");
-									System.out.println("[1]게임설명 [2]실행  [3]뒤로가기");
-									System.out.print("메뉴를 선택하세요 : ");
-									int menu4 = sc.nextInt();
-									 ru = new RuleCon();
-									if (menu4 == 1) {
-										ru.slotrule();
-
-									} else if (menu4 == 2) {
-
-									}
 									System.out.println();
 									BalanceController bc = new BalanceController();
 									UserVO uv = new UserVO(id);
@@ -119,16 +110,7 @@ public class Main {
 									System.out.println("====카드게임====");
 									System.out.println("[1]게임설명 [2]실행  [3]뒤로가기");
 									System.out.print("메뉴를 선택하세요 : ");
-									int menu5 = sc.nextInt();
-									ru = new RuleCon();
 
-									if (menu5 == 1) {
-										ru.cardrule();
-										System.out.println();
-
-									} else if (menu5 == 2) {
-
-									}
 								} else if (menu3 == 3) {
 									System.out.println("====잔액확인====");
 									BalanceController bc = new BalanceController();
@@ -157,9 +139,11 @@ public class Main {
 									System.out.println("올바른 번호를 입력해주세요.");
 								}
 							}
-
 						} else if (menu2 == 2) {
 							System.out.println("====룰 설명====");
+							RuleCon ru = new RuleCon();
+							ru.cardrule();
+							ru.slotrule();
 						} else if (menu2 == 3) {
 							System.out.println("메인 페이지로 이동합니다.");
 							break;
